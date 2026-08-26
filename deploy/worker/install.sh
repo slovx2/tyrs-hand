2deed62b5db4dfd47d73b1921167e45bf3dda0ae
song <slovx2@gmail.com>
2026-08-26 12:49:20 +0800

    fix: update worker installer protocol version

--- deploy/worker/install.sh
+++ deploy/worker/install.sh
@@ -247,7 +247,7 @@ fi
   if [ -n "${enrollment_token}" ]; then
     printf "TYRS_HAND_WORKER_ENROLLMENT_TOKEN='%s'\n" "${enrollment_token}"
   fi
-  printf "TYRS_HAND_WORKER_PROTOCOL_VERSION='28'\n"
+  printf "TYRS_HAND_WORKER_PROTOCOL_VERSION='29'\n"
   printf "TYRS_HAND_CODEX_BIN='%s'\n" "${worker_codex_bin}"
   printf "TYRS_HAND_WORKER_HOME='%s'\n" "${worker_home}"
   printf "TYRS_HAND_WORKER_CODEX_HOME='%s'\n" "${worker_codex_home}"
